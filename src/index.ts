ace04b2638d47b616de36169845a9f51ffc50db9
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2021-12-26 18:55:07 +0800

    feat: initial project config

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import solve from "./modules/solve.js";
+
+const result = solve('x^3+5x^2+2x+8');
+console.log(result);
---
+++ src/modules/solve.ts
@@ -0,0 +1,3 @@
+export default function solve(equation: string): number {
+  return 0;
+}
