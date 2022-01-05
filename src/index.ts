15894f901c8bae81be3675a48ba5b8547a4a4223
Juruel Keanu Lorenzo <keanulorenzo32@gmail.com>
2022-01-05 21:03:17 +0800

    fix(index): wrong operator on checker

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ client.post("/bisection", (req, res) => {
   try {
     const options = {} as Options;
 
-    if (typeof input.equation !== "string" && input.equation.length === 0) {
+    if (typeof input.equation !== "string" || input.equation.length === 0) {
       throw "Property 'equation' must be a string with a length greater than 1.";
     }
 
